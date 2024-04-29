9397ad467966c56c4fd64ca9fab3c773636e7fba
DandyYahmin <dandydarmawanalyahmin@gmail.com>
2024-04-29 20:48:14 +0700

    built navbar(undone)

---
+++ src/components/NavbarSection.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 right-0 px-5 py-3">
+        <logoSection class="text-5xl font-semibold text-slate-50 mulish">;</logoSection>
+    </div>
+</template>
