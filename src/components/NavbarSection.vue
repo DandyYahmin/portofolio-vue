2439fd2b26fef8ec170de8ec956fbb15eec39325
DandyYahmin <dandydarmawanalyahmin@gmail.com>
2024-05-05 21:29:54 +0700

    continue the navbar

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -4,6 +4,6 @@
 
 <template>
     <div class="fixed top-0 left-0 right-0 px-5 py-3">
-        <logoSection class="text-5xl font-semibold text-slate-50 mulish">;</logoSection>
+        <logoSection class="text-5xl font-semibold text-stone-700 mulish">;</logoSection>
     </div>
 </template>
